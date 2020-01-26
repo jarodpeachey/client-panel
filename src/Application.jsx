c893370de7892aa8a1780085e391afc325def9d3
Jarod Peachey <jwpeachey107@aol.com>
2020-01-26 18:40:10 -0500

    Minor updates

--- src/Application.jsx
+++ src/Application.jsx
@@ -2,14 +2,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
+// import { connect } from 'react-redux';
+// import { compose } from 'redux';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import styled, { ThemeProvider } from 'styled-components';
-import { firestoreConnect } from 'react-redux-firebase';
+// import { firestoreConnect } from 'react-redux-firebase';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faEdit, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
+// import { fab } from '@fortawesome/free-brands-svg-icons';
 import theme from './mui-theme';
 import styledTheme from './styled-theme';
 import Header from './components/Header';
